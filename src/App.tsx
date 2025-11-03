ba7993e9dfabbeb597c719719190fa0782aadc68
Guilherme Virgilio Picinin Oliveira Simões <m31249@almg.rede>
2025-11-03 17:57:15 -0300

    <feat> cap 1.1 props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
 
+function TituloDoFormulario(props: {children: string}) {
+  return <h2>{props.children}</h2>
+}
+
+function Label(props: {children: string, htmlFor: string}){
+  const {children, htmlFor} = props;
+  return <label htmlFor={htmlFor}>{children}</label>
+}
+
 function FormularioDeEvento() {
   return(
     <form className='form-evento'>
-      <h2>Preencha para criar um evento:</h2>
+        <TituloDoFormulario>
+          Preencha para criar um evento.
+        </TituloDoFormulario>
       <fieldset>
-        <label htmlFor='nome'>Qual o nome do evento?</label>
-        <input type='text' id='nome'/>
+        <Label htmlFor='nome'>
+          Qual o nome do evento?
+        </Label>
+        <input type='text' id='nome' placeholder='Summer dev hits'/>
       </fieldset>
     </form>
   );
